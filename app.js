2c1e5319486992768c38618456c4aa871fa0d211
unknown <nkumarabu@translucentcomputing.com>
2020-02-13 20:34:03 -0500

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://neraj:neraj@cluster0-brbg7.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://neraj:neraj@cluster0-brbg7.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
